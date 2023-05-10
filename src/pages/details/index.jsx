3b233cd38dd31a62e41e5eded0081f8a5f581b5f
Arthur Machado Wiebusch <arthur.wieb@gmail.com>
2023-05-09 21:26:02 -0700

    Continuaçao aula

--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -1,9 +1,10 @@
 import { Container } from "./styles";
-
+import { Button } from '../../components/button';
 export function Details() {
   return (
     <Container>
       <h1>Hello World</h1>
+      <Button/>
     </Container>
   );
 }
--- src/styles/global.js
+++ src/styles/global.js
@@ -5,9 +5,34 @@ export default createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+
     };
 
     body {
-        background-color: ${({ theme }) => theme.COLORS.BACKGROUNG_800};
+        background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+        color: ${({ theme }) => theme.COLORS.WHITE};
+        -webkit-font-smoothing: antialiased;
     };
+
+    body, input, button, textarea {
+        font-family: 'Roboto slab', sans-serif;
+        font-size: 16px;
+        outline: none
+    };
+    
+
+    a {
+        text-decoration: none;
+    };
+
+    button, a {
+        cursor: pointer;
+        transition: filter 0.2s;
+    }
+
+    button:hover, a:hover {
+        filter: brightness(0.9);
+    }
+
+
 `;
