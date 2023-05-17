4917791651711c6163ac6d5508711bbdb0593eba
Arthur Machado Wiebusch <arthur.wieb@gmail.com>
2023-05-16 21:03:22 -0700

    alvin é legal

--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -2,11 +2,16 @@ import { Container, Links } from "./styles";
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 import { Section } from '../../components/Section';
+import { Tag } from '../../components/Tag';
+import { ButtonText } from '../../components/ButtonText';
 
 export function Details() {
   return (
     <Container>
       <Header/>
+
+      <ButtonText title="Excluir Nota"/>
+
       <Section title="Links úteis">
         <Links>
        
@@ -14,6 +19,12 @@ export function Details() {
          
         </Links>
       </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express"/>
+        <Tag title="node"/>
+      </Section>
+
       
       <Button title="back"/>
     </Container>
