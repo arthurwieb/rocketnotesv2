0e8300732568856ffedea2cc0f74969965a46296
Arthur Machado Wiebusch <arthur.wieb@gmail.com>
2023-07-21 21:15:08 -0700

    file name

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,16 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import { New } from '../pages/New';
 import { Home } from '../pages/Home';
-import { Details } from '../pages/Details';
+import { Details } from '../pages/Details/index.jsx';
 import { Profile } from '../pages/Profile';
 
 export function AppRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/new" element={<New />}/>
-            <Route path="/profile" element={<Profile />}/>
-            <Route path="/details/:id" element={<Details />}/>
+            <Route path="/" element={<Home />} />
+            <Route path="/new" element={<New />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/details/:id" element={<Details />} />
         </Routes>
     )
 }
