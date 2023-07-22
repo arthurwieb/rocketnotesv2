5806d3a0f19ecb7be5dbf872b294484c901f9857
Arthur Machado Wiebusch <arthur.wieb@gmail.com>
2023-07-21 22:16:58 -0700

    debug folder

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { New } from '../pages/New';
 import { Home } from '../pages/Home';
-import { Details } from '../pages/Details';
+import { Details } from '../pages/Detailsf';
 import { Profile } from '../pages/Profile';
 
 export function AppRoutes() {
